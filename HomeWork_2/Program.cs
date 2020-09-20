7068dfb8a2bcd3ded875e9042796a9b3d919b560
Priboohnul <brodyachiy_pes@mail.ru>
2020-09-20 21:54:19 +1200

    Add Home Work 2

--- HomeWork_2/Program.cs
+++ HomeWork_2/Program.cs
@@ -10,6 +10,29 @@ namespace HomeWork_2
     {
         static void Main(string[] args)
         {
+            Console.Write("Enter triangle size: ");
+            int triangleSize = int.Parse(Console.ReadLine());
+            Console.Clear();
+
+            for (int i = 0; i <= triangleSize; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.SetCursorPosition(j, i);
+                    Console.Write("#");
+
+                    Console.SetCursorPosition(j, triangleSize * 2 + 3 - i);
+                    Console.Write("#");
+
+                    Console.SetCursorPosition(triangleSize - i + j, triangleSize * 2 + 4 + i);
+                    Console.Write("#");
+
+                    Console.SetCursorPosition(triangleSize - i + j, triangleSize * 4 + 7 - i);
+                    Console.Write("#");
+                }
+            }
+
+            Console.SetCursorPosition(0, triangleSize * 4 + 10);
         }
     }
 }
